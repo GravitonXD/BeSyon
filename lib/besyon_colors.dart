b48cb395bc2cf60645569d61b1dfd1c2adca8f1d
GravitonXD <jmolarte@up.edu.ph>
2023-03-05 20:17:27 +0800

    Initial

---
+++ lib/besyon_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class BesyonColors {
+  static Color grey = const Color.fromARGB(255, 217, 217, 217);
+  static Color purple = const Color.fromARGB(255, 157, 68, 240);
+  static Color blue = const Color.fromARGB(255, 51, 153, 255);
+}
